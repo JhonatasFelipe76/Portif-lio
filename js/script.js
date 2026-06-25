531dfb92f988365d1d18266ba157dc32dbddc4ca
JhonatasFelipe76 <jhonatasestudos571@gmail.com>
2026-06-25 21:39:06 +0100

    Adiçào da função limparMensagem e Alteração da função enviarMensagem

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 // pega o formulário
 let form = document.getElementById("formContato");
 
+
 // Quando o botão enviar é apertado a função enviar mensagem é chamada
 form.addEventListener("submit", enviarMensagem);
+//Função que limpa mensagem
+function limparMensagem(){
+    setTimeout(function (){
+        let mensagemValida = document.getElementById("mensagemValida");
+
+        mensagemValida.innerHTML = "";
+        mensagemValida.className = "";
+    }, 2000);
+}
+
+
 
 // função que válida os campos e envia a mensagem
 function enviarMensagem(enviar){
@@ -13,21 +25,31 @@ function enviarMensagem(enviar){
     let email = document.getElementById("email").value;
     let mensagem = document.getElementById("mensagem").value;
 
+    let mensagemValida = document.getElementById("mensagemValida");
+
     // verifica se os campos estão vazios
     if (nome == "" || email == "" || mensagem == "") {
-        alert("Os campos devem ser preenchidos!")
+        mensagemValida.className = "mensagemErro";
+        mensagemValida.innerHTML = "Todos os campos devem estar preenchidos!";
+
+        limparMensagem();
     
     } // verifica se o email está válido
     else if (email.indexOf("@") == -1 || email.indexOf(".") == -1){
-        alert("Digite um e-mail válido!")
-
+        mensagemValida.className = "mensagemErro";
+        mensagemValida.innerHTML = "Digite um e-mail válido";
+        limparMensagem();
    
     }  // avisa ao usuário que sua mensagem foi enviada
     else {
-        alert("Mensagem enviada com sucesso!");
+        mensagemValida.className = "mensagemSucesso";
+        mensagemValida.innerHTML = "Mensagem enviada com sucesso!";
+        limparMensagem();
 
         form.reset(); // reseta o formulario após ele ser enviado
     }
+
+
 
 
 };
